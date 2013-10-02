3f6b00930407a12f16688b11bd04adbcea8497fa
pahanez <pahanez@gmail.com>
2013-10-02 22:56:37 +0300

    two menu with one object

--- src/com/example/slidingmenutemplate/MainActivity.java
+++ src/com/example/slidingmenutemplate/MainActivity.java
@@ -1,27 +1,25 @@
 package com.example.slidingmenutemplate;
 
-import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
-
 import android.os.Bundle;
-import android.app.Activity;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+
 public class MainActivity extends FragmentActivity {
-	
+
 	private SlidingMenu mSlidingMenuLeft;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		
+
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		
+
 		initActionBarAndSlidingMenu();
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
@@ -30,8 +28,36 @@ public class MainActivity extends FragmentActivity {
 
 	private void initActionBarAndSlidingMenu() {
 		getActionBar().setHomeButtonEnabled(true);
-		
-		
+
+		mSlidingMenuLeft = new SlidingMenu(this);
+		mSlidingMenuLeft.setMode(SlidingMenu.LEFT_RIGHT);
+		mSlidingMenuLeft.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+		mSlidingMenuLeft.setShadowWidthRes(R.dimen.shadow_width);
+		mSlidingMenuLeft.setShadowDrawable(R.drawable.shadow);
+		mSlidingMenuLeft.setBehindOffsetRes(R.dimen.slidingmenu_offset);
+		mSlidingMenuLeft.setFadeDegree(0.35f);
+		mSlidingMenuLeft.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
+		mSlidingMenuLeft.setMenu(R.layout.menu_frame_one);
+		getSupportFragmentManager().beginTransaction()
+				.replace(R.id.menu_frame_one, new SampleListFragment())
+				.commit();
+
+		mSlidingMenuLeft.setSecondaryMenu(R.layout.menu_frame_two);
+
+		getSupportFragmentManager().beginTransaction()
+				.replace(R.id.menu_frame_two, new SampleListFragment())
+				.commit();
+		mSlidingMenuLeft.setSecondaryShadowDrawable(R.drawable.shadow);
+
+	}
+
+	@Override
+	public void onBackPressed() {
+		if (mSlidingMenuLeft.isMenuShowing()) {
+			mSlidingMenuLeft.showContent();
+		} else {
+			super.onBackPressed();
+		}
 	}
 
 }
